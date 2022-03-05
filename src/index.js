d73ee477e4d6c6f85c124dbf66b00b0abbcf1dfb
Piloniak <xxziomcioxx@gmail.com>
2022-03-06 00:44:56 +0100

    krok 2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+class Main extends React.Component{
+  render(){
+    const gry=["Beholder","Minecraft","Monopoly","Losowa","Gra","Planszowa","Na","Planszeo"]
+    return(
+      <div>
+        {Nav}
+        <SortingButtons/>
+        {gry.map((gra)=>
+        <Game name={gra} key={gra}/>)}
+      </div>
+    );
+  }
+}
+
+class SortingButtons extends React.Component{
+  render(){
+    const miesiace=["styczen","luty","marzec","kwiecien","maj","czerwiec","lipiec","sierpien","wrzesien","pazdziernik","listopad","grudzien"];
+    return(
+      <div className='sortingButtons'>
+       {miesiace.map((miesiac)=>
+        <Button month={miesiac} key={miesiac}/>
+       )}
+      </div>
+    );
+  }
+}
+
+
+class Button extends React.Component{
+  render(){
+    return(
+      <button>{this.props.month}</button>
+    )
+  }
+}
+
+class Game extends React.Component{
+  render(){
+    return(
+      <h3>{this.props.name}</h3>
+    )
+  }
+}
+
+const Nav = <h1>Planszeo</h1>;
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Main/>,
   document.getElementById('root')
 );
 
