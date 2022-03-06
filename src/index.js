235798bafc09accacdd6d4dbb21293357ba83eba
Piloniak <xxziomcioxx@gmail.com>
2022-03-06 23:19:43 +0100

    krok5

--- src/index.js
+++ src/index.js
@@ -3,73 +3,87 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+const gry=[{name:"Beholder",month:"marzec",id:0,opis:"Opis gry"},{name:"Minecraft",month:"styczen",id:1,opis:"Opis gry"},{name:"Monopoly",month:"pazdziernik",id:2,opis:"Opis gry"},{name:"Losowa",month:"listopad",id:3,opis:"Opis gry"},{name:"Gra",month:"sierpien",id:4,opis:"Opis gry"},{name:"Planszowa",month:"czerwiec",id:5,opis:"Opis gry"},{name:"Na",month:"maj",id:6,opis:"Opis gry"},{name:"Planszeo",month:"maj",id:7,opis:"Opis gry"}];
+
 class Main extends React.Component{
   constructor(props){
     super(props);
     this.state={
-      games: []
+      
+      months: ["styczen","luty","marzec","kwiecien","maj","czerwiec","lipiec","sierpien","wrzesien","pazdziernik","listopad","grudzien"]
     }
   }
 
   nazwyGier(month){
-    const gry=[{name:"Beholder",month:"marzec",id:0},{name:"Minecraft",month:"styczen",id:1},{name:"Monopoly",month:"pazdziernik",id:2},{name:"Losowa",month:"listopad",id:3},{name:"Gra",month:"sierpien",id:4},{name:"Planszowa",month:"czerwiec",id:5},{name:"Na",month:"maj",id:6},{name:"Planszeo",month:"maj",id:7}];
-    
     return(
     
-    gry.map((gra, index)=>{
-      <h1 key={index+gra.name}>{gra.month}</h1>
-      if(gra.month==month)
-        return <Game name={gra.name} key={gra.name}/> })
+    gry.map((gra)=>{
+      <h1 key={gra.id}>{gra.month}</h1>
+      if(gra.month===month)
+        return <Game name={gra.name} key={gra.id} opis={gra.opis}/> } )
+      
      
     )}
 
+  
+
+  sortClick(miesiac){
+  
+    let miesiace=this.state.months;
+    for(let i=0;i++;i<12){
+      miesiace[i]=miesiac;
+    }
+    
+    this.setState({months: miesiace}); 
+  
+  }
+
   render(){
     const miesiace=["styczen","luty","marzec","kwiecien","maj","czerwiec","lipiec","sierpien","wrzesien","pazdziernik","listopad","grudzien"];
+    
+    let temporary=this.state.months;
+
     return(
+      
       <div>
         {Nav}
-        <SortingButtons/>
+        <div className='sortingButtons'>
         {miesiace.map((miesiac)=>
+        <Button key={miesiac} month={miesiac} /*click={this.sortClick(miesiac).bind}*//>
+        )}
+        </div>
+        {temporary.map((miesiac)=>
         this.nazwyGier(miesiac))}       
       </div>
     );
   }
 }
 
-
-
-class SortingButtons extends React.Component{
-  render(){
-    const miesiace=["styczen","luty","marzec","kwiecien","maj","czerwiec","lipiec","sierpien","wrzesien","pazdziernik","listopad","grudzien"];
-    return(
-      <div className='sortingButtons'>
-       {miesiace.map((miesiac)=>
-        <Button month={miesiac} key={miesiac}/>
-       )}
-      </div>
-    );
-  }
-}
-
-
 class Button extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
   render(){
     return(
-      <button>{this.props.month}</button>
+      <button /*onClick={this.props.click(this.props.month)}*/ className='przycisk'>{this.props.month}</button>
     )
   }
 }
+//sortClick(this.props.month,this.props.months)
 
 class Game extends React.Component{
   render(){
     return(
+      <div className='gra'>
       <h3>{this.props.name}</h3>
-      
+      <p>{this.props.opis}</p>
+      </div>
     )
   }
 }
 
-const Nav = <h1>Planszeo</h1>;
+const Nav = <h1 className='tytul'>Planszeo</h1>;
 
 ReactDOM.render(
   <Main/>,
